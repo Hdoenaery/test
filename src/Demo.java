0b60336fc60e751204b0d78b01cd71f3e24f204f
Hdoenaery <1766508828@qq.com>
2024-09-01 15:18:17 +0800

    update2 Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,19 +1,17 @@
 import java.util.Random;
-
 public class Demo {
     public static void function1(){
         Random random = new Random();
-        int a = random.nextInt() % 1000;
-        int b = random.nextInt() % 1000;
+        System.out.println("1");
+        System.out.println("2");
+        int a = random.nextInt() % 500;
+        int b = random.nextInt() % 500;
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-        if(a > b)
-            System.out.println("a>b");
-        else if(a == b)
-            System.out.println("a=b");
-        else {
+        if(a >= b)
+            System.out.println("a>=b");
+        else
             System.out.println("a<b");
-        }
     }
 
     public static void main(String[] args){
