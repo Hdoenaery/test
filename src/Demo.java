6a58d49518f310837d4f33dc9328c9dcbb1c4326
Hdoenaery <1766508828@qq.com>
2024-09-01 14:46:33 +0800

    add Demo.java

---
+++ src/Demo.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Demo {
+    public static void function1(){
+        Random random = new Random();
+        int a = random.nextInt() % 1000;
+        int b = random.nextInt() % 1000;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        if(a > b)
+            System.out.println("a>b");
+        else if(a < b)
+            System.out.println("a<b");
+        else {
+            System.out.println("a=b");
+        }
+    }
+
+    public static void main(String[] args){
+        function1();
+    }
+}
